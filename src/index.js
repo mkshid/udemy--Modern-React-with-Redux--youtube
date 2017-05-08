9e28db90090573df158c1d9effaac17dd14cfc91
M. Hassan Khurshid <mkshid@gmail.com>
2017-05-08 14:41:49 +0200

    Change app to a class based component and add YTvidoes in state

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 import { API_KEY } from './settings';
 
-const App = () => {
+class App extends Component {
+
+    constructor(props){
+      super(props);
+
+      this.state = {videos: []};
+
+      YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+        this.setState({ videos });
+      });
+
+    }
+
+  render(){
     return (
-            <div>
-               <SearchBar />
-            </div>
+        <div>
+        <SearchBar />
+        </div>
     );
+  }
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
