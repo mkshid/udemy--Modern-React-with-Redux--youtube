712139470555836188f9c4fcbf16bcb4794fe120
M. Hassan Khurshid <mkshid@gmail.com>
2017-05-08 17:28:31 +0200

    Move selectedVideo into the main state

--- src/index.js
+++ src/index.js
@@ -12,10 +12,17 @@ class App extends Component {
     constructor(props){
       super(props);
 
-      this.state = {videos: []};
+      this.state = {
+        videos: [],
+        selectedVideo: null
+      };
 
       YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-        this.setState({ videos });
+        this.setState({
+          videos: videos,
+          selectedVideo: videos[0]
+        });
+
       });
 
     }
@@ -24,7 +31,7 @@ class App extends Component {
     return (
         <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
+        <VideoDetail video={this.state.selectedVideo}/>
         <VideoList videos={this.state.videos} />
         </div>
     );
