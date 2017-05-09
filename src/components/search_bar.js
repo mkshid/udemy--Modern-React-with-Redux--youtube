0fb1d3a031c3f5a88a2adae7ff629e6761c9c2e5
M. Hassan Khurshid <mkshid@gmail.com>
2017-05-09 09:59:09 +0200

    Add callback to add ad term

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,21 +2,26 @@ import React, {Component} from 'react';
 
 class SearchBar extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {term: ''};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {term: ''};
+  }
+
+  render() {
     
-    render() {
-        
-        return (
-            <div className="search-bar">
-                <input
-                 value={this.state.term}
-                 onChange={event => this.setState({term: event.target.value})}/>
-            </div>
-        );
-    }
+    return (
+      <div className="search-bar">
+        <input
+           value={this.state.term}
+           onChange={event => this.onInputChange(event.target.value)}/>
+      </div>
+    );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -17,20 +17,23 @@ class App extends Component {
         selectedVideo: null
       };
 
-      YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-        this.setState({
-          videos: videos,
-          selectedVideo: videos[0]
-        });
+      this.videoSearch('surfboards');
+    }
 
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
       });
 
-    }
+    });
+  }
 
   render(){
     return (
         <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
            videos={this.state.videos}
